054e0d93925d266d26342a9c9e49d21cea398c7b
Thiago César <thiago.or.code@gmail.com>
2023-07-28 00:24:41 -0300

    layout
    #beauty

--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -55,7 +55,7 @@
                     <div id="collapseBeauty" class="collapse" aria-labelledby="headingBeauty" data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
                             <a class="collapse-item" href="/vendas/1">Minhas Vendas</a>
-                            <a class="collapse-item" href="{{ url('/onemotos/' . auth()->id()) }}" target="_BLANK">Vender</a>
+                            <a class="collapse-item" href="{{ url('/onebeauty/' . auth()->id()) }}" target="_BLANK">Vender</a>
                         </div>
                     </div>
                 </li>
