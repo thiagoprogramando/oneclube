16944375ac4947c899d40276dc493593d515a84a
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2023-10-05 13:47:46 -0300

    up

--- app/Http/Controllers/BancoDoBrasilController.php
+++ app/Http/Controllers/BancoDoBrasilController.php
@@ -8,27 +8,28 @@ use GuzzleHttp\Client;
 class BancoDoBrasilController extends Controller
 {
     public function geraToken() {
-        $client = new Client();
+        var_dump(openssl_get_cert_locations());
+        // $client = new Client();
 
-        $data = [
-            'form_params' => [
-                'grant_type' => 'client_credentials',
-                'scope' => 'cobrancas.boletos-info, cobrancas.boletos-requisicao',
-            ],
-            'headers' => [
-                'Authorization' => 'Basic ZXlKcFpDSTZJbUZrT1Rka01EZ3RZV0k1TXkwME9HVTRMVGcyWWpJdFlqVTNOemN3SWl3aVkyOWthV2R2VUhWaWJHbGpZV1J2Y2lJNk1Dd2lZMjlrYVdkdlUyOW1kSGRoY21VaU9qYzFOREkwTENKelpYRjFaVzVqYVdGc1NXNXpkR0ZzWVdOaGJ5STZNWDA6ZXlKcFpDSTZJbUpoWkRJeE1qYzRMV0V3TkRFdE5EQmxZUzA0TkROakxUUXpZMkUxTXpNek1qVXhOQ0lzSW1OdlpHbG5iMUIxWW14cFkyRmtiM0lpT2pBc0ltTnZaR2xuYjFOdlpuUjNZWEpsSWpvM05UUXlOQ3dpYzJWeGRXVnVZMmxoYkVsdWMzUmhiR0ZqWVc4aU9qRXNJbk5sY1hWbGJtTnBZV3hEY21Wa1pXNWphV0ZzSWpveExDSmhiV0pwWlc1MFpTSTZJbWh2Ylc5c2IyZGhZMkZ2SWl3aWFXRjBJam94TmprMk1ERXpOekE1TnpVMWZR',
-                'Content-Type' => 'application/x-www-form-urlencoded',
-            ],
-        ];
+        // $data = [
+        //     'form_params' => [
+        //         'grant_type' => 'client_credentials',
+        //         'scope' => 'cobrancas.boletos-info, cobrancas.boletos-requisicao',
+        //     ],
+        //     'headers' => [
+        //         'Authorization' => 'Basic ZXlKcFpDSTZJbUZrT1Rka01EZ3RZV0k1TXkwME9HVTRMVGcyWWpJdFlqVTNOemN3SWl3aVkyOWthV2R2VUhWaWJHbGpZV1J2Y2lJNk1Dd2lZMjlrYVdkdlUyOW1kSGRoY21VaU9qYzFOREkwTENKelpYRjFaVzVqYVdGc1NXNXpkR0ZzWVdOaGJ5STZNWDA6ZXlKcFpDSTZJbUpoWkRJeE1qYzRMV0V3TkRFdE5EQmxZUzA0TkROakxUUXpZMkUxTXpNek1qVXhOQ0lzSW1OdlpHbG5iMUIxWW14cFkyRmtiM0lpT2pBc0ltTnZaR2xuYjFOdlpuUjNZWEpsSWpvM05UUXlOQ3dpYzJWeGRXVnVZMmxoYkVsdWMzUmhiR0ZqWVc4aU9qRXNJbk5sY1hWbGJtTnBZV3hEY21Wa1pXNWphV0ZzSWpveExDSmhiV0pwWlc1MFpTSTZJbWh2Ylc5c2IyZGhZMkZ2SWl3aWFXRjBJam94TmprMk1ERXpOekE1TnpVMWZR',
+        //         'Content-Type' => 'application/x-www-form-urlencoded',
+        //     ],
+        // ];
 
-        $response = $client->request('POST', 'https://oauth.sandbox.bb.com.br/oauth/token', $data);
-        if ($response->getStatusCode() == 200) {
-            $responseData = json_decode($response->getBody(), true);
-            $accessToken = $responseData['access_token'];
+        // $response = $client->request('POST', 'https://oauth.sandbox.bb.com.br/oauth/token', $data);
+        // if ($response->getStatusCode() == 200) {
+        //     $responseData = json_decode($response->getBody(), true);
+        //     $accessToken = $responseData['access_token'];
 
-            return $accessToken;
-        } else {
-            return false;
-        }
+        //     return $accessToken;
+        // } else {
+        //     return false;
+        // }
     }
 }
