dda37fc9148010cb7598e288397acb9c21b249ed
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2023-10-05 12:13:43 -0300

    projeto-base-bb
    # Gera Token

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
 use App\Http\Controllers\AsaasController;
+use App\Http\Controllers\BancoDoBrasilController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -25,6 +15,8 @@ Route::post('clicksing', [AsaasController::class, 'enviaLinkPagamento'])->name('
 Route::post('assas', [AsaasController::class, 'receberPagamento'])->name('assas');
 //Planos One Clube
 Route::post('oneclube', [AsaasController::class, 'geraAssasOneClube'])->name('oneclube');
+//Gera Token BB
+Route::get('/tokenBB', [BancoDoBrasilController::class, 'geraToken'])->name('tokenBB');
 
 
 // Route::post('/geraPagamento', [AsaasController::class, 'geraPagamento'])->name('geraLink');
