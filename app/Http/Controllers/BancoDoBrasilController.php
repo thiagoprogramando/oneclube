f498d9e7edaf36d5dd0954e5a7ba50cd4b50ee54
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2023-10-05 13:57:40 -0300

    up

--- app/Http/Controllers/BancoDoBrasilController.php
+++ app/Http/Controllers/BancoDoBrasilController.php
@@ -8,7 +8,7 @@ use GuzzleHttp\Client;
 class BancoDoBrasilController extends Controller
 {
     public function geraToken() {
-        $client = new Client(['verify' => '/opt/alt/openssl11/etc/pki/tls/cert.pem']);
+        $client = new Client();
 
         $data = [
             'form_params' => [
@@ -19,6 +19,7 @@ class BancoDoBrasilController extends Controller
                 'Authorization' => 'Basic ZXlKcFpDSTZJbUZrT1Rka01EZ3RZV0k1TXkwME9HVTRMVGcyWWpJdFlqVTNOemN3SWl3aVkyOWthV2R2VUhWaWJHbGpZV1J2Y2lJNk1Dd2lZMjlrYVdkdlUyOW1kSGRoY21VaU9qYzFOREkwTENKelpYRjFaVzVqYVdGc1NXNXpkR0ZzWVdOaGJ5STZNWDA6ZXlKcFpDSTZJbUpoWkRJeE1qYzRMV0V3TkRFdE5EQmxZUzA0TkROakxUUXpZMkUxTXpNek1qVXhOQ0lzSW1OdlpHbG5iMUIxWW14cFkyRmtiM0lpT2pBc0ltTnZaR2xuYjFOdlpuUjNZWEpsSWpvM05UUXlOQ3dpYzJWeGRXVnVZMmxoYkVsdWMzUmhiR0ZqWVc4aU9qRXNJbk5sY1hWbGJtTnBZV3hEY21Wa1pXNWphV0ZzSWpveExDSmhiV0pwWlc1MFpTSTZJbWh2Ylc5c2IyZGhZMkZ2SWl3aWFXRjBJam94TmprMk1ERXpOekE1TnpVMWZR',
                 'Content-Type' => 'application/x-www-form-urlencoded',
             ],
+            'verify' => false
         ];
 
         $response = $client->request('POST', 'https://oauth.sandbox.bb.com.br/oauth/token', $data);
