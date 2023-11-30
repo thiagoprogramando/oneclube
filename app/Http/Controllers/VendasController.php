94afcb9711c7755c8fbcc3bcf8338049eca5a8db
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2023-11-30 17:55:45 -0300

    up

--- app/Http/Controllers/VendasController.php
+++ app/Http/Controllers/VendasController.php
@@ -228,31 +228,37 @@ class VendasController extends Controller {
     }
 
     public function notificarSignatario($contrato, $telefone) {
-
         $client = new Client();
-
-        $url = 'https://api.z-api.io/instances/3C71DE8B199F70020C478ECF03C1E469/token/DC7D43456F83CCBA2701B78B/send-link';
-
-        $response = $client->post($url, [
-            'headers' => [
-                'Content-Type'  => 'application/json',
-                'Accept'        => 'application/json',
-                'Client-Token'  => 'Fabe25dbd69e54f34931e1c5f0dda8c5bS'
-            ],
-            'json' => [
-                'phone'           => '55' . $telefone,
-                'message'         => "Prezado Cliente, segue seu contrato de adesão ao produto da G7 Assessoria: \r\n \r\n",
-                'image'           => 'https://grupo7assessoria.com.br/wp-content/uploads/2023/07/Copia-de-MULTISERVICOS-250-%C3%97-250-px-2.png',
-                'linkUrl'         => $contrato,
-                'title'           => 'Assinatura de Contrato',
-                'linkDescription' => 'Link para Assinatura Digital'
-            ],
-        ]);
-
-        $responseData = json_decode($response->getBody(), true);
-        if (isset($responseData['id'])) {
-            return true;
-        } else {
+    
+        $url = env('API_URL_ZAPI') . '/send-link';
+    
+        try {
+            $response = $client->post($url, [
+                'headers' => [
+                    'Content-Type'  => 'application/json',
+                    'Accept'        => 'application/json',
+                    'Client-Token'  => 'Fabe25dbd69e54f34931e1c5f0dda8c5bS',
+                ],
+                'json' => [
+                    'phone'           => '55' . $telefone,
+                    'message'         => "Prezado Cliente, segue seu contrato de adesão ao produto da G7 Assessoria: \r\n \r\n",
+                    'image'           => 'https://grupo7assessoria.com.br/wp-content/uploads/2023/07/Copia-de-MULTISERVICOS-250-%C3%97-250-px-2.png',
+                    'linkUrl'         => $contrato,
+                    'title'           => 'Assinatura de Contrato',
+                    'linkDescription' => 'Link para Assinatura Digital',
+                ],
+            ]);
+    
+            $responseData = json_decode($response->getBody(), true);
+    
+            if (isset($responseData['id'])) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (\Exception $e) {
+            // Exibir detalhes do erro
+            dd($e->getMessage(), $e->getCode());
             return false;
         }
     }
