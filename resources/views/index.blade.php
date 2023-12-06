d60479f735ea71cdde908f2f21b91a0b8a4f7437
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2023-12-06 13:04:52 -0300

    up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,9 +2,9 @@
 @section('conteudo')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-xl-10 col-lg-12 col-md-9 mt-5">
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card o-hidden border-0 shadow-lg my-5 mt-5">
                     <div class="card-body p-0">
                         <div class="row">
                             <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
@@ -21,12 +21,6 @@
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user" name="password" placeholder="Senha">
                                         </div>
-                                        <!--<div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Lembrar login</label>
-                                            </div>
-                                        </div>-->
                                         <button type="submit" class="btn btn-primary btn-user btn-block"> Login </button>
                                     </form>
                                     <hr>
