2ff1c06513ac057492865bce8ff28d6e948d76d3
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2023-12-08 13:25:33 -0300

    up

--- app/Http/Controllers/Gatway/AssasController.php
+++ app/Http/Controllers/Gatway/AssasController.php
@@ -15,57 +15,105 @@ use Carbon\Carbon;
 
 class AssasController extends Controller {
 
-    public function invoiceSale($id) {
+    // public function invoiceSale($id) {
 
+    //     $sale = Sale::find($id);
+    //     $customer = $this->createCustomer($sale->name, $sale->cpfcnpj);
+    //     if($customer) {
+
+    //         $invoiceCount = 0; //FATURA 0
+    //         $installmentCount = $sale->installmentCount; //PARCELAMENTO
+
+    //         $initialPayment = 300; //VALOR FATURA INICIAL CASO PARCELAMENTO
+            
+    //         $dueDate = now()->addDay(); //PRIMEIRO VENCIMENTO
+    //         $description = "Serviços & Consultoria G7"; //DESCRIÇÃO FATURA
+
+    //         while ($invoiceCount < $installmentCount) {
+                
+    //             //CASO ESTEJA NA SEGUNDA FATURA, ACRESCENTAR UM MÊS
+    //             if ($invoiceCount > 0) {
+    //                 $dueDate->addMonth();
+    //             }
+
+    //             //CASO NÃO TENHA PARCELAMENTO, ENVIAR O VALOR TOTAL
+    //             if ($installmentCount == 0 || $installmentCount == 1) {
+    //                 $charge = $this->createCharge($customer, $sale->billingType, $sale->value, $description, $dueDate);
+    //             }
+
+    //             //CASO TENHA PARCELAMENTO
+    //             if($installmentCount > 1) {
+    //                 if($invoiceCount >= 1) {
+    //                     $valuePerInstallment = ($sale->value - $initialPayment) / ($sale->installmentCount - 1);
+    //                     $charge = $this->createCharge($customer, $sale->billingType, $valuePerInstallment, $description, $dueDate);
+    //                 }
+    //                 $charge = $this->createCharge($customer, $sale->billingType, 300, $description, $dueDate);
+    //             }
+                
+    //             if ($charge) {
+
+    //                 $invoice                = new Invoice();
+    //                 $invoice->idUser        = $sale->id;
+    //                 $invoice->name          = "Parcela N° " . ($invoiceCount + 1);
+    //                 $invoice->description   = $description;
+    //                 $invoice->token         = $charge['id'];
+    //                 $invoice->url           = $charge['invoiceUrl'];
+    //                 if($installmentCount == 0 || $installmentCount == 1) {
+    //                     $invoice->value         = $sale->value;
+    //                 } else {
+    //                     $invoice->value         = ($invoiceCount == 0 || $invoiceCount == 1) ? $initialPayment : $valuePerInstallment;
+    //                 }
+    //                 $invoice->status        = "PENDING_PAY";
+    //                 $invoice->type          = 3;
+    //                 $invoice->dueDate       = $dueDate;
+    //                 $invoice->save();
+    
+    //                 $invoiceCount++;
+    //             }
+    //         }
+    
+    //         return true;
+    //     }
+    
+    //     return false;
+    // }
+
+    public function invoiceSale($id) {
         $sale = Sale::find($id);
         $customer = $this->createCustomer($sale->name, $sale->cpfcnpj);
+    
         if($customer) {
-
-            $invoiceCount = 0; //FATURA 0
-            $installmentCount = $sale->installmentCount; //PARCELAMENTO
-
-            $initialPayment = 300; //VALOR FATURA INICIAL CASO PARCELAMENTO
-            
-            $dueDate = now()->addDay(); //PRIMEIRO VENCIMENTO
-            $description = "Serviços & Consultoria G7"; //DESCRIÇÃO FATURA
-
+            $invoiceCount = 0;
+            $installmentCount = $sale->installmentCount;
+            $initialPayment = 300;
+    
+            $dueDate = now()->addDay();
+            $description = "Serviços & Consultoria G7";
+    
             while ($invoiceCount < $installmentCount) {
-                
-                //CASO ESTEJA NA SEGUNDA FATURA, ACRESCENTAR UM MÊS
                 if ($invoiceCount > 0) {
                     $dueDate->addMonth();
                 }
-
-                //CASO NÃO TENHA PARCELAMENTO, ENVIAR O VALOR TOTAL
-                if ($installmentCount == 0 || $installmentCount == 1) {
-                    $charge = $this->createCharge($customer, $sale->billingType, $sale->value, $description, $dueDate);
+    
+                if ($installmentCount <= 1) {
+                    $chargeValue = $sale->value;
+                } else {
+                    $chargeValue = ($invoiceCount == 0) ? $initialPayment : (($sale->value - $initialPayment) / ($installmentCount - 1));
                 }
-
-                //CASO TENHA PARCELAMENTO
-                if($installmentCount > 1) {
-                    if($invoiceCount >= 1) {
-                        $valuePerInstallment = ($sale->value - $initialPayment) / ($sale->installmentCount - 1);
-                        $charge = $this->createCharge($customer, $sale->billingType, $valuePerInstallment, $description, $dueDate);
-                    }
-                    $charge = $this->createCharge($customer, $sale->billingType, 300, $description, $dueDate);
-                }
-                
+    
+                $charge = $this->createCharge($customer, $sale->billingType, $chargeValue, $description, $dueDate);
+    
                 if ($charge) {
-
-                    $invoice                = new Invoice();
-                    $invoice->idUser        = $sale->id;
-                    $invoice->name          = "Parcela N° " . ($invoiceCount + 1);
-                    $invoice->description   = $description;
-                    $invoice->token         = $charge['id'];
-                    $invoice->url           = $charge['invoiceUrl'];
-                    if($installmentCount == 0 || $installmentCount == 1) {
-                        $invoice->value         = $sale->value;
-                    } else {
-                        $invoice->value         = ($invoiceCount == 0 || $invoiceCount == 1) ? $initialPayment : $valuePerInstallment;
-                    }
-                    $invoice->status        = "PENDING_PAY";
-                    $invoice->type          = 3;
-                    $invoice->dueDate       = $dueDate;
+                    $invoice = new Invoice();
+                    $invoice->idUser = $sale->id;
+                    $invoice->name = "Parcela N° " . ($invoiceCount + 1);
+                    $invoice->description = $description;
+                    $invoice->token = $charge['id'];
+                    $invoice->url = $charge['invoiceUrl'];
+                    $invoice->value = $chargeValue;
+                    $invoice->status = "PENDING_PAY";
+                    $invoice->type = 3;
+                    $invoice->dueDate = $dueDate;
                     $invoice->save();
     
                     $invoiceCount++;
@@ -77,6 +125,7 @@ class AssasController extends Controller {
     
         return false;
     }
+    
     
     public function invoiceCreate(Request $request) {
 
