5563211e14cc1ab1e202cd3eaefabf3169bef3b5
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2023-12-08 12:13:44 -0300

    up

--- resources/views/dashboard/wallet/wallet.blade.php
+++ resources/views/dashboard/wallet/wallet.blade.php
@@ -14,7 +14,7 @@
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-green text-uppercase mb-1">
                                     Saldo (Disponível para saque)</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">R$ 215,000</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">R$ {{ $balance }}</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -31,7 +31,7 @@
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                     Saldo (Recebíveis)</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">R$ 40,000</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">R$ {{ $statistics }}</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-calendar fa-2x text-gray-300"></i>
